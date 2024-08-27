249976670a4cfc464d10fed6614238c6b005df5e
Emanuel Suriano <emanuel.suriano@revolut.com>
2024-08-27 16:34:50 +0200

    base?

--- astro.config.ts
+++ astro.config.ts
@@ -5,7 +5,6 @@ import tailwind from '@astrojs/tailwind'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://franpacelli.github.io',
-  base: 'astro-portfolio',
 
   integrations: [react(), tailwind({ applyBaseStyles: false })],
 })
